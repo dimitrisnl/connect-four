9983b2962192dd9c003f7f7959109cf7ff765d33
Dimitrios Lytras <dnlytras@gmail.com>
2018-08-03 22:39:27 +0300

    Updates

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,25 @@
 const senseJoystick = require('sense-joystick')
-import { Game } from './models'
-import { changePlayer, generatePlayers, setPixels } from './utils'
+import { Game, Player } from './models'
+import { RED_COLOR, GREEN_COLOR, setPixels } from './utils'
 
 const Board = new Game()
-const players = generatePlayers(2)
-let currentPlayer = 0
+const players = [new Player(RED_COLOR), new Player(GREEN_COLOR)]
+let currentPlayerIdx = 0
 
-Board.setIndicator(players[currentPlayer].color)
+Board.setIndicator(players[currentPlayerIdx].color)
 setPixels(Board.state)
 
 senseJoystick.getJoystick().then((joystick: any) => {
   joystick.on('press', (value: string) => {
     if (value === 'click') {
+      console.log(value)
       if (!Board.isValid()) return false
 
-      Board.apply(players[currentPlayer].play())
-      currentPlayer = changePlayer(currentPlayer, players)
+      Board.apply(players[currentPlayerIdx].play())
+      currentPlayerIdx = 1 - currentPlayerIdx
 
       if (Board.hasWin()) Board.clear()
-      Board.setIndicator(players[currentPlayer].color)
+      Board.setIndicator(players[currentPlayerIdx].color)
     } else Board.moveIndicator(value)
     setPixels(Board.state)
   })
